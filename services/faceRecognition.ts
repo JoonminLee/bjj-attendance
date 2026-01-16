f6c585598bb0298543d711dbc916a03a06259d58
JoonminLee <jmandoo0524@gmail.com>
2026-01-16 14:47:40 +0900

    v0.1

--- services/faceRecognition.ts
+++ services/faceRecognition.ts
@@ -1,7 +1,8 @@
 
 import * as faceapi from 'face-api.js';
 
-const MODEL_URL = '/models';
+// @ts-ignore
+const MODEL_URL = import.meta.env.BASE_URL + 'models';
 
 let isModelsLoaded = false;
 let modelLoadingPromise: Promise<void> | null = null;
