67470eea53369cc71db7e10f37398827112a35dd
JoonminLee <jmandoo0524@gmail.com>
2026-01-16 17:47:25 +0900

    v0.1

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   // GitHub Pages 배포 시 레포지토리 이름을 base로 설정해야 합니다.
   // 레포지토리 이름이 'oss-system'이라면 '/oss-system/'으로 수정하세요.
   // 여기서는 상대 경로인 './'를 사용하여 범용적으로 작동하게 합니다.
-  base: './',
+  base: '/bjj-attendance/',
   plugins: [react()],
   build: {
     outDir: 'dist',
